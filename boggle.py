fc0cdf5aebf261ed16c9a2a00c2089559ae80d10
emiliaaflalo <emilia.aflalo@mail.huji.ac.il>
2020-01-20 17:26:49 +0200

    change

--- boggle.py
+++ boggle.py
@@ -1,11 +1,13 @@
 import boggle_board_randomizer as random
 import tkinter as tk
 import time
-BOARD_HEIGHT = 100
-BOARD_WIDTH = 100
+BOARD_HEIGHT = 500
+BOARD_WIDTH = 500
+BOGGLE_SIDE = 4
+
 
 def create_word_list(filename):
-    f = open("boggle_dict.txt", "r")
+    f = open(filename, "r")
     legal_words = [line.strip("\n") for line in f]
     return legal_words
 
@@ -22,19 +24,47 @@ def create_board():
     return board_list
 
 
+def create_placements():
+    list_of_place =[]
+    start_x = 150
+    start_y = 150
+    for i in range(BOGGLE_SIDE):
+        for j in range(BOGGLE_SIDE):
+            list_of_place.append((start_x, start_y))
+            start_x += 50
+        start_y += 50
+    return list_of_place
+
+
+
 class Board:
     def __init__(self, letter_list):
         self.letter_list = letter_list
-        self.timer = timer
-        self.score = score
+        self.timer = 3
+        self.score = 0
         self.right_words = []
         self.root = tk.Tk()
         self.height = BOARD_HEIGHT
         self.width = BOARD_WIDTH
 
+    def create_boggle_buttons(self):
+        dic_of_butts = {}
+        placements = create_placements()
+        counter = 1
+        for row in self.letter_list:
+            for letter in row:
+                cur_button = tk.Button(self.root, text=letter, height=50, width=50, font=('Helvetica', 20))
+                dic_of_butts['button'+str(counter)] = cur_button
+                cur_button.pack()
+                cur_button.place(x=placements[counter-1][0], y=placements[counter-1][1])
+                counter += 1
+
+
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.resizable(width=False, height=False)
+    board_game = Board(create_board())
+    board_game.root.geometry('500x500')
+    board_game.create_boggle_buttons()
+    board_game.root.resizable(width=False, height=False)
+    board_game.root.mainloop()
 
-    root.mainloop()
